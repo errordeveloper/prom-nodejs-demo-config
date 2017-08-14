64c8e06bfb5165cf6f9c8d7a7cb9ce6e2239e498
Ilya Dmitrichenko <errordeveloper@gmail.com>
2017-08-14 11:33:02 +0100

    Use `prom-run`

--- load-test-build.rb
+++ load-test-build.rb
@@ -1,16 +1,20 @@
-from 'alpine'
+def install_packages
+  run 'apk add --update curl apache2-utils'
+  run 'rm -rf /var/cache/apk/*'
+end
 
-run 'apk add --update curl apache2-utils'
-run 'rm -rf /var/cache/apk/*'
+from 'tomwilkie/prom-run'
+
+install_packages
 
 cmds = {
-  slow: "while true; do (ab -n 1000 -c 80 http://nodejs-demo/; sleep 30;); done",
-  fast: "while true; do (ab -n 500 -c 40 http://nodejs-demo/; sleep 60;); done",
+  slow: %w(-period 30s ab -n 1000 -c 80 http://nodejs-demo/),
+  fast: %w(-period 60s ab -n 500 -c 40 http://nodejs-demo/),
 }
 
 flatten
 
 cmds.each do |tag,cmd|
-  set_exec entrypoint: %w(sh -c), cmd: [cmd]
+  set_exec entrypoint: %w(/prom-run), cmd: cmd
   tag "docker.io/errordeveloper/prom-nodejs-demo-load-test:#{tag}"
 end
